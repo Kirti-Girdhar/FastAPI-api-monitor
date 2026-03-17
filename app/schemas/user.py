c74ec7a0b8d20f7b20070578844e35eda7ff7dfd
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-17 12:27:27 +0530

    Add comment: created schema for user

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        from_attributes = True
