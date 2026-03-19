0bb397a4bd3050a8880db1503270c6da6eb4592e
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-19 10:47:39 +0530

    Feature: Add Token and TokenData schemas for JWT authentication

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -13,3 +15,10 @@ class UserLogin(BaseModel):
 
     class Config:
         from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type:str
+
+class TokenData(BaseModel):
+    email: Optional[str]= None
