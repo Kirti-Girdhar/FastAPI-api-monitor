b8130c3f55fb6f9c8343b92af833c3eaf1849806
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-19 10:48:17 +0530

    Feature: Implement JWT token validation and OAuth2 dependency

---
+++ app/utils/dependencies.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from ..database import get_db
+from app.models.users import User
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from app.utils.security import SECRET_KEY, ALGORITHM
+from app.schemas.user import TokenData
+
+
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token:str=Depends(oauth2_scheme),db: Session= Depends(get_db)):
+    try:
+        print(token)
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        mail=payload.get("sub")
+        if mail is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        token_data=TokenData(email=mail)
+        curr_user=db.query(User).filter(User.email== token_data.email).first()
+
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    return curr_user
