647351ce937bd84beb22e352ad0216af7146c983
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-19 10:49:25 +0530

    Feature: Initialize FastAPI app with database setup and router includes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.routers import auth, endpoints
+from app import models
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(endpoints.router)
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
