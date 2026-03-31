1edb8261c915c76addb52935665e577c53b5beb9
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-31 13:12:07 +0530

    feat: add authentication to get_alerts endpoint and filter alerts by current user

--- app/routers/alerts.py
+++ app/routers/alerts.py
@@ -2,13 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.alert import Alert
+from app.models.endpoint import Endpoint
+from app.models.users import User
+from app.utils.dependencies import get_current_user
 
 router=APIRouter(
     prefix="/alerts",
     tags=["Alerts"]
 )
 
+# 
 @router.get("/")
-def get_alerts(db:Session=Depends(get_db)):
-    alerts=db.query(Alert).all()
+def get_alerts(db:Session=Depends(get_db),current_user: User=Depends(get_current_user)):
+
+    alerts=db.query(Alert).join(Endpoint).filter(Endpoint.user_id == current_user.id).all()
     return alerts
