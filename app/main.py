18002a14166f60e559937b8d62f69586607d41f2
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-03-31 13:11:44 +0530

    feat: integrate monitoring scheduler startup event and add stats router to FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routers import auth, endpoints, alerts
+from app.routers import auth, endpoints, alerts, stats
+from app.scheduler.monitor_scheduler import start_scheduler,scheduler
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    start_scheduler()
+
+
 app.include_router(auth.router)
 app.include_router(endpoints.router)
 app.include_router(alerts.router)
+app.include_router(stats.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"} 
+
+# @app.on_event("shutdown")
+# async def shutdown_event():
+#     print("Shutting down scheduler...")
+#     scheduler.shutdown()
