fd0a8c42f9bf5a507e4253b1e0accd8733c3453d
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2026-04-01 12:15:44 +0530

    Add authentication and authorization to stats endpoint

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
+from app.models.users import User
+from app.models.endpoint import Endpoint
 from app.services.stats_service import get_endpoint_stats
+from app.utils.dependencies import get_current_user
 
 router=APIRouter(
     prefix="/stats",
@@ -9,6 +12,9 @@ router=APIRouter(
 )
 
 @router.get("/endpoint/{endpoint_id}")
-def endpoint_stats(endpoint_id: int, db: Session = Depends(get_db)):
+def endpoint_stats(endpoint_id: int, db: Session = Depends(get_db), current_user :User= Depends(get_current_user)):
+    endpoint = db.query(Endpoint).filter(Endpoint.id == endpoint_id, Endpoint.user_id == current_user.id).first()
+    if not endpoint:
+        raise HTTPException(status_code=404, detail="Endpoint not found")
     stats= get_endpoint_stats(endpoint_id,db)
     return stats
